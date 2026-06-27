eb55611c229723e82184516b35dd494535add2b4
endl24 <rkdendl22@gmail.com>
2026-06-27 13:47:09 +0900

    feat: set up router configuration

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,40 @@
 import { createBrowserRouter } from 'react-router-dom';
-
 import { App } from '../app/App';
+import { AuthPage } from '../features/auth/AuthPage';
+import { BlogPage } from '../features/blog/BlogPage';
+import { HomePage } from '../features/home/HomePage';
+import { JourneyPage } from '../features/journey/JourneyPage';
+import { ProfilePage } from '../features/profile/ProfilePage';
+import { TimelinePage } from '../features/timeline/TimelinePage';
+import { ROUTES } from '../shared/constants/routes';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: ROUTES.root,
     element: <App />,
+  },
+  {
+    path: ROUTES.auth,
+    element: <AuthPage />,
+  },
+  {
+    path: ROUTES.home,
+    element: <HomePage />,
+  },
+  {
+    path: ROUTES.timeline,
+    element: <TimelinePage />,
+  },
+  {
+    path: ROUTES.journey,
+    element: <JourneyPage />,
+  },
+  {
+    path: ROUTES.profile,
+    element: <ProfilePage />,
+  },
+  {
+    path: ROUTES.blog,
+    element: <BlogPage />,
   },
 ]);
