f961b0571cea191e7c086215e84fc32f77172c98
endl24 <rkdendl22@gmail.com>
2026-07-03 16:42:55 +0900

    feat: add journey mock data

---
+++ src/mocks/journeyData.ts
@@ -0,0 +1,36 @@
+import { Journey } from '../features/journey/types/journey';
+
+export const journeyData: Journey[] = [
+  {
+    id: 1,
+    title: "영국 4/7 동선",
+    date: "2024년 4월 7일",
+    savedAt: "2024. 4. 7",
+    places: [
+      { id: 1, name: "장소1" },
+      { id: 2, name: "장소2" },
+      { id: 3, name: "장소3" },
+    ],
+  },
+  {
+    id: 2,
+    title: "두번 배낭",
+    date: "2024년 4월 7일 - 4월 9일",
+    savedAt: "2024. 4. 7",
+    places: [
+      { id: 1, name: "장소1" },
+      { id: 2, name: "장소2" },
+    ],
+  },
+  {
+    id: 3,
+    title: "영국 4/7 동선",
+    date: "2024년 4월 7일",
+    savedAt: "2024. 4. 7",
+    places: [
+      { id: 1, name: "장소1" },
+      { id: 2, name: "장소2" },
+      { id: 3, name: "장소3" },
+    ],
+  },
+];
